1da4409476b46a77f5b7ce6e9c4b27e1f82d2eb0
LuisDPieri <luisdpieri@gmail.com>
2025-06-13 13:24:02 -0300

    Adiciona CV com HTML, CSS e JS

---
+++ assets/principal.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('darkmode') === 'true') {
+    document.body.classList.add('darkmode');
+  }
+});
+
+const btnToggle = document.getElementById('darkmode');
+btnToggle.addEventListener('click', () => {
+  document.body.classList.toggle('darkmode');
+  localStorage.setItem('darkmode', document.body.classList.contains('darkmode'));
+});
+
+document.getElementById("baixar-cv").addEventListener("click", () => {
+  const element = document.getElementById("curriculo");
+  const opt = {
+    margin:   0.5,
+    filename: "Curriculo_Luis_Felipe_de_Pieri.pdf",
+    image:    { type: "jpeg", quality: 0.98 },
+    html2canvas: { 
+      scale: 2,
+      useCORS: true,
+      onclone: (clonedDoc) => {
+        clonedDoc.body.classList.remove("darkmode");
+      }
+    },
+    jsPDF:    { unit: "in", format: "a4", orientation: "portrait" },
+    pagebreak: {
+      mode:  ["css", "legacy"],
+      avoid: ["section", "h3"]
+    }
+  };
+
+  html2pdf()
+    .set(opt)
+    .from(element)
+    .save();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const secoes = [
+    { id: "inicio", texto: "Início"},
+    { id: "rp", texto: "Resumo" },
+    { id: "ep", texto: "Experiência" },
+    { id: "ht", texto: "Habilidades" },
+    { id: "fa", texto: "Formação" },
+    { id: "cc", texto: "Cursos" },
+    { id: "idi", texto: "Idiomas" }
+  ];
+
+  const nav = document.getElementById("menu-fixo");
+
+  secoes.forEach(secao => {
+    const link = document.createElement("a");
+    link.href = `#${secao.id}`;
+    link.textContent = secao.texto;
+    link.setAttribute("aria-label", `Ir para a seção ${secao.texto}`);
+    link.title = `Ir para ${secao.texto}`;
+    nav.appendChild(link);
+  });
+});
